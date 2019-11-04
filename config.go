94e1b1bb57147f18db9ce9a55dabb664b828fc0a
Naoki Kishi <naokikishi.kyoto@gmail.com>
2019-11-04 20:41:29 +0900

    Add reading environment variable function

---
+++ config.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+// Config represents config values read from the environment variables
+type Config struct {
+	GitHubAccessToken  string `envconfig:"MIKKU_GITHUB_ACCESS_TOKEN" required:"true"`
+	GitHubOwner        string `envconfig:"MIKKU_GITHUB_OWNER" required:"true"`
+	ManifestRepository string `envconfig:"MIKKU_MANIFEST_REPOSITORY"`
+	ManifestFilepath   string `envconfig:"MIKKU_MANIFEST_FILEPATH"`
+	DockerImageName    string `envconfig:"MIKKU_DOCKER_IMAGE_NAME"`
+}
+
+// ReadConfig reads config values from the environment variables
+func ReadConfig() (*Config, error) {
+	cfg := &Config{}
+	if err := envconfig.Process("", cfg); err != nil {
+		return nil, fmt.Errorf("failed to read environment variables: %w", err)
+	}
+	return cfg, nil
+}
---
+++ config_test.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestReadConfig(t *testing.T) {
+	tests := []struct {
+		name    string
+		setEnv  func() func()
+		want    *Config
+		wantErr bool
+	}{
+		{
+			name: "no environment variable set",
+			setEnv: func() func() {
+				return func() {}
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "GitHub access token doesn't be set",
+			setEnv: func() func() {
+				_ = os.Setenv("MIKKU_GITHUB_OWNER", "MIKKU_GITHUB_OWNER")
+				return func() {
+					_ = os.Unsetenv("MIKKU_GITHUB_OWNER")
+				}
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "GitHub owner doesn't be set",
+			setEnv: func() func() {
+				_ = os.Setenv("MIKKU_GITHUB_ACCESS_TOKEN", "MIKKU_GITHUB_ACCESS_TOKEN")
+				return func() {
+					_ = os.Unsetenv("MIKKU_GITHUB_ACCESS_TOKEN")
+				}
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "All env config be set",
+			setEnv: func() func() {
+				_ = os.Setenv("MIKKU_GITHUB_ACCESS_TOKEN", "MIKKU_GITHUB_ACCESS_TOKEN")
+				_ = os.Setenv("MIKKU_GITHUB_OWNER", "MIKKU_GITHUB_OWNER")
+				_ = os.Setenv("MIKKU_MANIFEST_REPOSITORY", "MIKKU_MANIFEST_REPOSITORY")
+				_ = os.Setenv("MIKKU_MANIFEST_FILEPATH", "MIKKU_MANIFEST_FILEPATH")
+				_ = os.Setenv("MIKKU_DOCKER_IMAGE_NAME", "MIKKU_DOCKER_IMAGE_NAME")
+				return func() {
+					_ = os.Unsetenv("MIKKU_GITHUB_ACCESS_TOKEN")
+					_ = os.Unsetenv("MIKKU_GITHUB_OWNER")
+					_ = os.Unsetenv("MIKKU_MANIFEST_REPOSITORY")
+					_ = os.Unsetenv("MIKKU_MANIFEST_FILEPATH")
+					_ = os.Unsetenv("MIKKU_DOCKER_IMAGE_NAME")
+				}
+			},
+			want: &Config{
+				GitHubAccessToken:  "MIKKU_GITHUB_ACCESS_TOKEN",
+				GitHubOwner:        "MIKKU_GITHUB_OWNER",
+				ManifestRepository: "MIKKU_MANIFEST_REPOSITORY",
+				ManifestFilepath:   "MIKKU_MANIFEST_FILEPATH",
+				DockerImageName:    "MIKKU_DOCKER_IMAGE_NAME",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer tt.setEnv()()
+
+			got, err := ReadConfig()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReadConfig() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !cmp.Equal(got, tt.want) {
+				t.Errorf("ReadConfig() diff=%s", cmp.Diff(got, tt.want))
+			}
+		})
+	}
+}
